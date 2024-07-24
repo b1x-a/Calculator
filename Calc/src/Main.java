e09a061083816d6009e0118c4c314f0060d64ac5
Pavel Biksaliev <pavelbev@gmail.com>
2024-07-24 18:58:16 +0300

    Коммит1

--- Calc/src/Main.java
+++ Calc/src/Main.java
@@ -1,15 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(calc(sc.nextLine()));}
+
+    public static String calc(String input) throws Exception {
+        String[] words = input.split(" ");
+        if (words.length != 3) throw new Exception("Неверная длина строки.");
+
+        int firstNumber = Integer.parseInt(words[0]);
+        if (firstNumber < 1 || firstNumber > 10) throw new Exception("Число должно быть в диапазоне от 1 до 10");
+
+        int secondNumber = Integer.parseInt(words[2]);
+        if (secondNumber < 1 || secondNumber > 10) throw new Exception("Число должно быть в диапозоне от 1 до 10");
+
+        String mathOperation = words[1];
+
+        int result = switch (mathOperation) {
+            case "+" -> firstNumber + secondNumber;
+            case "-" -> firstNumber - secondNumber;
+            case "*" -> firstNumber * secondNumber;
+            case "/" -> firstNumber / secondNumber;
+            default -> throw new IllegalStateException("формат математической операции не удовлетворяет заданию");
+        };
+        return String.valueOf(result);
+
     }
 }
